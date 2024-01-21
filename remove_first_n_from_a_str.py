07d15b61b7f237b49bd5b0888dc5f76d8807798b
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-21 23:31:36 +0800

    pseudocode

--- remove_first_n_from_a_str.py
+++ remove_first_n_from_a_str.py
@@ -1,2 +1,8 @@
 # Exercise 4: Remove first n characters from a string
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+# pseudocode
+# 1. print "Removing characters from a string"
+# 2. def function(pynative, number)
+# 3.    print orig string
+# 4.    print removed string
